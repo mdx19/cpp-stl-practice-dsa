eb95e893805d6ab158cb99f48c1d14544e335b75
Madhav Chotaliya <madhavchotaliya319@gmail.com>
2026-07-01 21:06:32 +0530

    Your commit message

--- p1.cpp
+++ p1.cpp
@@ -1,7 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main(){
-  
 
+int main() {
 
+    vector<int> v = {1, 2, 4, 4, 4, 6, 8};
 
+    auto it = lower_bound(v.begin(), v.end(), 4);
+
+    cout << *it;
+}
